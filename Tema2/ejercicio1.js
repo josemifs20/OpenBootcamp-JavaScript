6fb1f00299987449a787bbcb71f112ae8aa7b994
Josemi <fs.josemiguel@gmail.com>
2022-10-05 10:47:09 +0200

    Ejercicio Tema2

---
+++ Tema2/ejercicio1.js
@@ -0,0 +1,14 @@
+const arr = [
+    "José Miguel FS",
+    31,
+    true,
+    new Date(1991,7,20),
+    {
+        titulo: "ESDLA",
+        autor: "Patric",
+        fecha: new Date(1996,0,1),
+        url: "www.google.com"
+    }
+];
+
+console.log(arr);
