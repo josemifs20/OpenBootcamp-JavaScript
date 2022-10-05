3e55bd273c226b53086dafb5a73feea10d507d6b
Josemi <fs.josemiguel@gmail.com>
2022-10-05 17:38:59 +0200

    Ejercicio Tema 5

---
+++ Tema5/ejercicio.js
@@ -0,0 +1,6 @@
+let altura = 191;
+let alturaFloat = 1.91;
+let peso = 92.65;
+let alturaCeil = Math.ceil(alturaFloat);
+let pesoFloor = Math.floor(peso);
+let max = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
